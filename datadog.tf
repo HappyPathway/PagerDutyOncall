f86a38932233c734014de41a59e9fcb48292939e
Dave Arnold <dja+djaboxx@sonic.net>
2018-05-06 13:39:50 -0700

    Update datadog.tf

--- datadog.tf
+++ datadog.tf
@@ -4,6 +4,6 @@ data "pagerduty_vendor" "datadog" {
 
 resource "pagerduty_service_integration" "datadog" {
   name    = "${data.pagerduty_vendor.datadog.name}"
-  service = "${"pagerduty_service.service.id}"
+  service = "${pagerduty_service.service.id}"
   vendor  = "${data.pagerduty_vendor.datadog.id}"
 }
